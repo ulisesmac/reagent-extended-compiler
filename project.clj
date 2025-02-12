f8a8ee14a3a6a599a5ab9f8c28df7c50be2bd334
Ulises M <ulises.ssb@hotmail.com>
2025-02-12 08:47:23 -0600

    add group name

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject reagent-extended-compiler "0.1.0-SNAPSHOT"
+(defproject com.github.ulisesmac/reagent-extended-compiler "0.1.0-SNAPSHOT"
   :description "An extended reagent compiler to customize the developer experience"
   :url "https://github.com/ulisesmac/reagent-extended-compiler"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
