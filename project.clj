caa79a1dc8cde8aaead851abbff7e43077bca059
Ulises M <ulises.ssb@hotmail.com>
2025-02-12 08:23:22 -0600

    Add url

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject reagent-extended-compiler "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "An extended reagent compiler to customize the developer experience"
+  :url "https://github.com/ulisesmac/reagent-extended-compiler"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :plugins [[lein-cljsbuild "1.1.8"]]
