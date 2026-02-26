a6bf8877125b648a41636c2aaec2fc87271da01b
Ulises M <ulises.ssb@hotmail.com>
2026-02-26 03:21:09 -0600

    Add add-styles macro to merge React Native styles

--- src/reagent_extended_compiler/utils/transforms.cljs
+++ src/reagent_extended_compiler/utils/transforms.cljs
@@ -6,7 +6,26 @@
 
 (def map-array transforms-impl/map-array)
 
-(defn ->js-prop-obj [m]
+(defn ->js-prop-obj
   "Transforms a cljs map into a JS object with keys as props style.
    Recursive. Runtime. Fast - Cached."
+  [m]
   (prop-converter/convert-custom-prop-value template/*current-default-compiler* m))
+
+(defn- compose-styles-process [result entry]
+  (if (vector? entry)
+    (reduce compose-styles-process result entry)
+    (conj! result entry)))
+
+(defn add-styles
+  "Compose a vector of style entries from various runtime values. Accepts
+  maps, JS objects, vectors (recursively flattened), and ignores nil/false
+  entries. Returns a vector suitable for React Native's style prop.
+
+  Optimized to minimize intermediate allocations when flattening nested
+  style vectors."
+  [& styles]
+  (persistent!
+   (reduce compose-styles-process
+           (transient [])
+           styles)))
