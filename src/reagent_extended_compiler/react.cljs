59f0a213bbb7a0b651843e54087e2b19c8f9ccd8
Ulises M <ulises.ssb@hotmail.com>
2026-03-13 23:06:21 -0600

    Simplify start-trip code

--- src/reagent_extended_compiler/react.cljs
+++ src/reagent_extended_compiler/react.cljs
@@ -1,6 +1,7 @@
 (ns reagent-extended-compiler.react
   (:require
-   ["react" :refer [useEffect useState useCallback useMemo useRef]]))
+   ["react" :refer [useEffect useState useCallback useMemo useRef]]
+   [reagent-extended-compiler.utils.transforms :as transforms]))
 
 (defn- fn-wrapper [f]
   (fn []
@@ -9,19 +10,27 @@
         result
         js/undefined))))
 
+(defn dep-value [x]
+  (if (keyword? x) (name x) x))
+
+(defn deps-array
+  "Convert hook deps to a JS array, naming keywords and leaving other values as-is."
+  [deps]
+  (transforms/map-array dep-value deps))
+
 (defn use-effect
   ([f]
    (useEffect (fn-wrapper f)))
   ([f deps]
-   (useEffect (fn-wrapper f) (clj->js deps))))
+   (useEffect (fn-wrapper f) (deps-array deps))))
 
 (defn use-callback [f deps]
-  (useCallback f (to-array deps)))
+  (useCallback f (deps-array deps)))
 
 (def use-state useState)
 
 (defn use-memo [f deps]
-  (useMemo f (to-array deps)))
+  (useMemo f (deps-array deps)))
 
 (defn use-ref [initial-value]
   ^js (useRef initial-value))
