c69c7f45fec855a8ddff34eb2652d49447e49f75
Ulises M <ulises.ssb@hotmail.com>
2026-04-08 21:59:53 -0600

    Add keyboard fns

--- src/reagent_extended/react_native.cljs
+++ src/reagent_extended/react_native.cljs
@@ -2,21 +2,37 @@
   (:require
    ["react-native" :refer [Dimensions Keyboard NativeModules Platform StyleSheet useColorScheme useWindowDimensions]]
    [applied-science.js-interop :as j]
+   [cljs-bean.core :refer [->clj]]
    [clojure.string :as string]))
 
 (def platform Platform)
-(def keyboard Keyboard)
-(def keyboard-dismiss! (.-dismiss Keyboard))
 (def platform-os (keyword "platform" (j/get platform :OS)))
 (def android? (= platform-os :platform/android))
 (def ios? (= platform-os :platform/ios))
+
+(def keyboard Keyboard)
+(defn keyboard-metrics []
+  (->clj (j/call keyboard :metrics)))
+(defn add-keyboard-listener! [evt f]
+  (j/call keyboard :addListener (name evt) f))
+(def keyboard-dismiss! (.-dismiss Keyboard))
+(defn keyboard-visible? []
+  (j/call keyboard :isVisible))
+
 (def style-sheet-absolute-fill (j/get StyleSheet :absoluteFill))
 
 (defn use-color-scheme []
   (let [color-scheme (or (useColorScheme) "light")]
     (keyword "theme" color-scheme)))
 
-(defn use-window-dimensions []
+(defn use-window-dimensions
+  "Returns the current React Native window dimensions as a map with `:width`,
+  `:height`, and `:scale`.
+
+  On Android, these window metrics are not guaranteed to be safe-area
+  normalized. Depending on the device, OEM, and navigation mode, `:height` may
+  or may not include areas covered by system bars."
+  []
   (let [dimensions (useWindowDimensions)]
     {:height (j/get dimensions :height)
      :scale  (j/get dimensions :scale)
